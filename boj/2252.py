6f82035aa7e23da0bed77e91a2f3d7fe563ba5aa
shineild <49579706+shineild@users.noreply.github.com>
2021-11-02 20:16:29 +0900

    Create 2252.py

---
+++ boj/2252.py
@@ -0,0 +1,40 @@
+import sys
+from collections import deque
+
+n, m = map(int, sys.stdin.readline().split())
+indegree = [0] * (n+1)
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    indegree[b] += 1
+
+
+def topology_sort():
+    result = []  # 알고리즘 수행 결과를 담을 리스트
+    q = deque()  # 큐 기능을 위한 deque 라이브러리 사용
+
+    # 처음 시작할 때는 진입차수가 0인 노드를 큐에 삽입
+    for i in range(1, n + 1):
+        if indegree[i] == 0:
+            q.append(i)
+
+    # 큐가 빌 때까지 반복
+    while q:
+        # 큐에서 원소 꺼내기
+        now = q.popleft()
+        result.append(now)
+        # 해당 원소와 연결된 노드들의 진입차수에서 1 빼기
+        for i in graph[now]:
+            indegree[i] -= 1
+            # 새롭게 진입차수가 0이 되는 노드를 큐에 삽입
+            if indegree[i] == 0:
+                q.append(i)
+
+    # 위상 정렬을 수행한 결과 출력
+    for i in result:
+        print(i, end=' ')
+    print("")
+
+
+topology_sort()
